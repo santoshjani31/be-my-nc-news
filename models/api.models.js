5b3c7758abb4a71f4e3f9af8a26cd7c13679416e
santoshjani31 <santosh.jani@gmail.com>
2024-11-29 16:07:47 +0000

    add GET /api/articles/:article_id (comment_count) endpoint

--- models/api.models.js
+++ models/api.models.js
@@ -7,20 +7,21 @@ exports.fetchApiTopics = () => {
 };
 
 exports.fetchArticleById = (article_id) => {
-  return db
-    .query(
-      `
-    SELECT * FROM articles 
-    WHERE article_id = $1;
-    `,
-      [article_id]
-    )
-    .then(({ rows }) => {
-      if (!rows.length) {
-        return Promise.reject({ status: 404, msg: 'Article not found' });
-      }
-      return rows;
-    });
+  const queryStr = `
+    SELECT 
+      articles.*,
+      COUNT(comments.comment_id)::INT AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;`;
+
+  return db.query(queryStr, [article_id]).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: 'Article not found' });
+    }
+    return rows[0];
+  });
 };
 
 exports.fetchArticles = (sort_by = 'created_at', order = 'desc', topic) => {
